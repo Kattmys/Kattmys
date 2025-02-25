29cf23be2652e2cafe46a0a967ce145162688f7b
Woodpecker515 <francis.backstrom@gmail.com>
2025-02-25 23:52:28 +0100

    Download page halfway added + photos.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
 import json
-import pprint
 from flask import Flask, render_template
 
 app = Flask(__name__)
 with open("data/content.json", encoding="utf-8") as f:
     data = json.load(f)
-    pprint.pp(data)
 
 if __name__ == '__main__':
    app.run()
@@ -14,6 +12,26 @@ if __name__ == '__main__':
 @app.route("/")
 def home():
     return render_template(r"index.html", data=data)
+
+
+
+@app.route("/download/snake_jack.html")
+def snake_jack():
+    return render_template(r"download/snake_jack.html")
+
+@app.route("/download/snake_francis.html")
+def snake_francis():
+    return render_template(r"download/snake_francis.html")
+
+
+
+@app.route("/sigge")
+def katt_sigge():
+    return render_template(r"katter/sigge.html")
+
+@app.route("/bonzo")
+def katt_bonzo():
+    return render_template(r"katter/bonzo.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
