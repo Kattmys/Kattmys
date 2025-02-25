246eb8c9d16f5a6d85bdc5f6ba30c3c209cb443e
Woodpecker515 <francis.backstrom@gmail.com>
2025-02-25 21:39:46 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import json
+import pprint
+from flask import Flask, render_template
+
+app = Flask(__name__)
+with open("data/content.json", encoding="utf-8") as f:
+    data = json.load(f)
+    pprint.pp(data)
+
+if __name__ == '__main__':
+   app.run()
+
+@app.route("/")
+def home():
+    return render_template(r"index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
