a6aabd56ed9fc8f47d14f9abbebe979bdc484e1c
jackiboi307 <jack.josefsson@gmail.com>
2025-02-26 17:17:46 +0100

    La till data/downloads.json, en all.css, och uppdaterade download-sidan (WIP)

--- main.py
+++ main.py
@@ -3,27 +3,30 @@ import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 with open("data/content.json", encoding="utf-8") as f:
     data = json.load(f)
 
+with open("data/downloads.json", encoding="utf-8") as f:
+    downloads = json.load(f)
+
 if __name__ == '__main__':
    app.run()
+
+# Huvudsidan
 
 @app.route("/")
 def home():
     return render_template(r"index.html", data=data)
 
+# Nedladdningar
 
+for url in downloads:
+    app.route(f"/download/{url}.html")(
+        lambda: render_template(f"/download/{url}.html", data=downloads[url])
+    )
 
-@app.route("/download/snake_jack.html")
-def snake_jack():
-    return render_template(r"download/snake_jack.html")
-
-@app.route("/download/snake_francis.html")
-def snake_francis():
-    return render_template(r"download/snake_francis.html")
-
-
+# Katter
 
 @app.route("/sigge")
 def katt_sigge():
@@ -32,6 +35,8 @@ def katt_sigge():
 @app.route("/bonzo")
 def katt_bonzo():
     return render_template(r"katter/bonzo.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
