7314d940258fa737486c517bb164c9c9af2c26da
jackiboi307 <jack.josefsson@gmail.com>
2025-03-16 20:23:49 +0100

    la till 'pages' som genereras från markdown

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 import json
-from flask import Flask, render_template
 
-# apa
+from markdown import markdown # pip install markdown
+
+import flask
+# from flask import Flask, render_template
+Flask = flask.Flask
+render_template = flask.render_template
 
 app = Flask(__name__)
 
@@ -19,32 +23,47 @@ def home():
     return render_template(r"index.html", data=data)
 
 # Nedladdningar
+
 @app.route("/download/<file>")
-def download(file):
+def page_download(file):
     try:
         download_data = downloads[file]
         return render_template("/download.html", data=download_data)
 
     except KeyError:
-        return render_template(
-            "/download.html",
-            data={
-                "name": f"404 Invalid address '{file}'",
-                "error": True
-            })
+        # return render_template(
+        #     "/download.html",
+        #     data={
+        #         "name": f"404 Invalid address '{file}'",
+        #         "error": True
+        #     })
+
+        return flask.redirect('/404')
+
+# Småsidor
+
+@app.route("/page/<name>")
+def page_page(name):
+    try:
+        with open(f"static/pages/{name}.md") as file:
+            html = markdown(file.read())
+            print(html)
+            return render_template("/page.html").replace("REPLACE_THIS", html)
+
+    except FileNotFoundError:
+        return flask.redirect('/404')
 
 # Katter
 
 @app.route("/sigge")
-def katt_sigge():
+def page_katt_sigge():
     return render_template(r"katter/sigge.html")
 
 @app.route("/bonzo")
-def katt_bonzo():
+def page_katt_bonzo():
     return render_template(r"katter/bonzo.html")
 
-
+# Start
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(debug=True, host="0.0.0.0", port=50000)
---
+++ static/pages/om.md
@@ -0,0 +1,11 @@
+# Kattmysnätverket
+
+Det här är Kattmysnätverket. Det bästa nätverket i mannaminne.
+
+## Hejsan
+
+Jag heter Jack.
+
+- Katter
+- Bananer
+- Vaniljsås
